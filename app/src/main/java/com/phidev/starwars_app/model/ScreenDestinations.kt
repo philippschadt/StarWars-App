b4c61facdf12f624c81a389d3b9f31b49cea59f0
phidev <philippschadt@gmx.de>
2022-11-05 21:35:08 +0100

    Added navigation arguments. To get detailed information related to the name.

--- app/src/main/java/com/phidev/starwars_app/model/ScreenDestinations.kt
+++ app/src/main/java/com/phidev/starwars_app/model/ScreenDestinations.kt
@@ -1,6 +1,15 @@
 package com.phidev.starwars_app.model
 
 sealed class ScreenDestinations(val route: String){
-    object MainScreen: ScreenDestinations("main_screen")
-    object DetailScreen: ScreenDestinations("detail_screen")
+    object MainScreen: ScreenDestinations(route = "main_screen")
+    object DetailScreen: ScreenDestinations(route = "detail_screen")
+
+    fun withArgs(vararg args: String): String{
+        return buildString {
+            append(route)
+            args.forEach { arg->
+                append("/$arg")
+            }
+        }
+    }
 }
--- app/src/main/java/com/phidev/starwars_app/navigation/Navigation.kt
+++ app/src/main/java/com/phidev/starwars_app/navigation/Navigation.kt
@@ -1,22 +1,36 @@
 package com.phidev.starwars_app.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.phidev.starwars_app.model.ScreenDestinations
 import com.phidev.starwars_app.views.DetailScreen
-import com.phidev.starwars_app.views.MainScreen
+import com.phidev.starwars_app.views.main.MainScreen
 
 @Composable
-fun Navigation(){
+fun Navigation() {
+
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "main_screen"){
-        composable(ScreenDestinations.MainScreen.route){
-            MainScreen()
+    NavHost(navController = navController, startDestination = "main_screen") {
+        composable(ScreenDestinations.MainScreen.route) {
+            MainScreen(navController = navController)
         }
-        composable(ScreenDestinations.DetailScreen.route){
-            DetailScreen()
+        composable(
+            ScreenDestinations.DetailScreen.route + "/{name}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                    defaultValue = "No Information available"
+                    nullable = true
+                }
+            )
+        ) { entry ->
+            DetailScreen(
+                name = entry.arguments?.getString("name")
+            )
         }
     }
 }
